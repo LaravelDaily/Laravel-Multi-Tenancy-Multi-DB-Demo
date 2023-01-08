ef7f49fb2dc25f6ae5697de7cfe73306f439885a
Nerijus <krekas@gmail.com>
2023-01-08 22:23:34 +0200

    Fix views

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -9,18 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <x-auth-validation-errors class="mb-4" :errors="$errors"/>
-
                     <form method="POST" action="{{ route('projects.update', $project) }}">
                         @csrf
                         @method('PUT')
 
                         <!-- Name -->
                         <div>
-                            <x-label for="name" :value="__('Name')"/>
+                            <x-input-label for="name" :value="__('Name')"/>
 
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="$project->name"
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="$project->name"
                                      required autofocus/>
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
 
                         <div class="flex mt-4">
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -9,27 +9,27 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <x-auth-validation-errors class="mb-4" :errors="$errors"/>
-
                     <form method="POST" action="{{ route('tasks.store') }}">
                     @csrf
 
                     <!-- Name -->
                         <div>
-                            <x-label for="name" :value="__('Name')"/>
+                            <x-input-label for="name" :value="__('Name')"/>
 
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
                                      required autofocus/>
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
 
                         <div class="mt-4">
-                            <x-label for="project_id" :value="__('Project')"/>
+                            <x-input-label for="project_id" :value="__('Project')"/>
 
                             <x-select id="project_id" name="project_id" class="block mt-1 w-full">
                                 @foreach ($projects as $project)
                                     <option value="{{ $project->id }}">{{ $project->name }}</option>
                                 @endforeach
                             </x-select>
+                            <x-input-error :messages="$errors->get('project_id')" class="mt-2" />
                         </div>
 
                         <div class="flex mt-4">
